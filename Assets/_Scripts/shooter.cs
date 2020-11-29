a3158cb1ebeaf4e6ad3e06b2827e17b258ff60b7
Tabisz <maro.990@gmail.com>
2020-11-29 15:22:29 +0100

    Scripts cleaning.

--- Assets/_Scripts/shooter.cs
+++ Assets/_Scripts/shooter.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class shooter : MonoBehaviour
+public class Shooter : MonoBehaviour
 {
     [SerializeField]
     GameObject bulletPrefab;
